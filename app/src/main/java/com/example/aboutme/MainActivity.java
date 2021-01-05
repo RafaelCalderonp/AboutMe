b2866e58dcd940e157323ab53512e022bd5645f6
Rafael <rafael.calderonp@gmail.com>
2021-01-04 21:28:59 -0300

    Initial commit

---
+++ app/src/main/java/com/example/aboutme/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.aboutme;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.bumptech.glide.Glide;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        CollapsingToolbarLayout toolBarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        toolBarLayout.setTitle(getTitle());
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ContacActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageView imageView = findViewById(R.id.ivPortada);
+        Glide.with(this)
+                .load("https://unsplash.com/photos/zyvASd8PM9A/download?force=true&w=640")
+         .into(imageView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_scrolling, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    //   TODO 1  Arreglar la interface grafica respecto a lo solicitado
+    //  TODO 2 Tener una vista Landscape para la MainActivity
+    //  TODO 3 Opcional, Utilizar ViewBinding en toda la MainActivit
+
+}
